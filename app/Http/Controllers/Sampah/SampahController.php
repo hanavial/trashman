a680ab87dcaee7a84557a6a2c0c8e91f2b764b51
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2020-07-16 18:41:20 +0700

    Add function store in Sampah Controller

--- app/Http/Controllers/Sampah/SampahController.php
+++ app/Http/Controllers/Sampah/SampahController.php
@@ -8,5 +8,19 @@ use Illuminate\Http\Request;
 
 class SampahController extends Controller
 {
+    public function store()
+    {
+        $kategori = KategoriSampah::findOrFail(request('kategori_id'));
 
+        $sampah = Sampah::create([
+            'kategori_id' => $kategori->id,
+            'nama' => request('nama'),
+            'deskripsi' => request('deskripsi'),
+        ]);
+
+        return response()->json([
+            'message' => 'Sampah baru telah ditambahkan',
+            'sampah' => $sampah,
+        ]);
+    }
 }

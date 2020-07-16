7ffd36c52917f804720ade0b5578ac6a146981a9
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2020-07-16 18:04:43 +0700

    Add Api Kategori Sampah

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['namespace' => 'Sampah'], function () {
+
+    Route::group(['prefix' => 'sampah'], function () {
+        
+    });
+
+    Route::group(['prefix' => 'kategori'], function () {
+        Route::get('', 'KategoriSampahController@index');
+    });
 });

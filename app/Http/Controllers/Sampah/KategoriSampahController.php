8731cbfc0b2bbc93c7e94c186a071a6912628d3a
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2020-07-16 17:46:46 +0700

    Add Contoller

---
+++ app/Http/Controllers/Sampah/KategoriSampahController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Sampah;
+
+use App\Models\KategoriSampah;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KategoriSampahController extends Controller
+{
+    
+}
---
+++ app/Http/Controllers/Sampah/SampahController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Sampah;
+
+use App\Models\{Sampah, KategoriSampah};
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SampahController extends Controller
+{
+
+}

2c3ad2e602cdc19e2eb13b2dfab7a3bcbd51859d
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2020-07-17 23:45:32 +0700

    Edit Home vue

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -3,10 +3,10 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <a class="navbar-brand mx-auto">Data Sampah</a>
         </nav>
-        <div class="container py-4">
+        <div class="container py-3">
             <div class="row">
-                <div class="col-md-6">
-                    <router-link class="btn btn-block btn-primary py-2" exact :to="{name: 'create'}">Tambah</router-link>
+                <div class="col-md-12">
+                    <router-link class="btn btn-block btn-primary py-2 mb-3 shadow" exact :to="{name: 'create'}">Tambah</router-link>
                     <!-- <h1>Home</h1> -->
                     <div class="card mt-2">
                         <div class="card-body">
@@ -16,6 +16,16 @@
                     <div class="card mt-2">
                         <div class="card-body">
                             <p>Plastik PET</p>
+                        </div>
+                    </div>
+                    <div class="card mt-2">
+                        <div class="card-body">
+                            <p>Plastik Kresek</p>
+                        </div>
+                    </div>
+                    <div class="card mt-2">
+                        <div class="card-body">
+                            <p>Baja Ringan</p>
                         </div>
                     </div>
                 </div>
@@ -41,6 +51,13 @@ export default {
     .bg-primary{
         background-color: #0C325F !important;
     }
+    .card-body{
+        border: none;
+        border-radius: 100px;
+    }
+    .btn{
+        border-radius: 7px;
+    }
     .btn-primary{
         background-color: #30AEE4;
         border: none;
@@ -48,6 +65,9 @@ export default {
     .btn-primary:hover{
         background-color: #23a6df;
         border: none;
+    }
+    .shadow{
+        box-shadow: 0 0.9rem 1rem rgb(48, 174, 228, 0.15) !important;
     }
     /* #0C325F
         #30AEE4

5b5dada135015a6526087f3b2522fd078789ec65
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2020-07-17 11:52:48 +0700

    Add Vue as View

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// Route::view('/{any}', 'app')->where('any','.*');
+Route::view('/{any}', 'app')->where('any','.*');
 
 // ,
 // "repositories": [

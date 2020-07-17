5076d5e15e2f69018c62cbbf54a30cbe61a2ac72
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2020-07-17 13:16:56 +0700

    Edit Home vue

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,8 +1,16 @@
 <template>
-
-    <div class="container">
-        <h1>Home</h1>
-        <router-link class="nav-link" exact :to="{name: 'create'}">Create</router-link>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand mx-auto">Data Sampah</a>
+        </nav>
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Home</h1>
+                    <router-link class="nav-link" exact :to="{name: 'create'}">Create</router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 

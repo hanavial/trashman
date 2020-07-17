c4ef9acdeaa7dc96f534d9ff17ce54edbb0a9f1e
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2020-07-17 13:16:39 +0700

    Add form in Create Vue

--- resources/js/views/Create.vue
+++ resources/js/views/Create.vue
@@ -1,9 +1,29 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h2>Create</h2>
-                <router-link class="nav-link" exact :to="{name: 'home'}">Home</router-link>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand mx-auto">Input Sampah</a>
+        </nav>
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-md-6">
+                    <form action="">
+                        <div class="form-group">
+                            <label for="kategori">Kategori Sampah</label>
+                            <select id="subject" class="form-control">
+                                <option placeholder="Kategori Sampah">
+                                    Kategori
+                                </option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="nama">Nama Sampah</label>
+                            <input type="text"  id="title" class="form-control" placeholder="Nama Sampah">
+                            <!-- <div v-if="theErrors.title" class="mt-2 text-danger">{{ theErrors.title[0]}}</div> -->
+                        </div>
+                        <button type="submit" class="btn btn-primary d-flex align-item-center">Simpan</button>
+                    </form>
+                    <router-link class="nav-link" exact :to="{name: 'home'}">Home</router-link>
+                </div>
             </div>
         </div>
     </div>

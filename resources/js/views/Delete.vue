b515575ac40e88c9421d60e0055a5be416d5217b
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2020-07-18 23:01:20 +0700

    Add Delete View Component

---
+++ resources/js/views/Delete.vue
@@ -0,0 +1,25 @@
+<template>
+    <a ref="deleteSampah" @click="destroySampah" class="nav-link pull-right delete">Hapus</a>
+</template>
+
+<script>
+export default {
+    props: ['endpoint'],
+    methods: {
+        async destroySampah(){
+            let q = window.confirm("Apakah anda yakin ?");
+
+            if (q == true) {
+                let response = await axios.delete(`/api/sampah/${this.endpoint}/delete`);
+                if (response.status == 200) {
+                    this.$refs.deleteSampah.parentNode.parentNode.parentNode.remove();
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

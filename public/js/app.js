2d0bbc973b795592998f536f6b11d60df1dc7a7b
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2020-07-19 00:30:40 +0700

    Commit App

--- public/js/app.js
+++ public/js/app.js
@@ -2221,7 +2221,6 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 //
 //
 //
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   components: {
